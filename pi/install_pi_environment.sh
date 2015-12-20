95b3248eed902842d392e8d165b35c3ce8adebff
onionys <onionys@gmail.com>
2015-12-20 12:05:58 +0000

    deleted:    install_pi_environment.sh 	renamed:    gvimrc -> linux/gvimrc 	new file:   pi/bashrc 	modified:   pi/install_pi_environment.sh 	deleted:    pi_vimrc

--- pi/install_pi_environment.sh
+++ pi/install_pi_environment.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-## editor tools
-apt-get install vim ctags
+#echo "Add ftp.yzu.edu.tw site into apt source.list"
+#sed -i "1ideb ftp://ftp.yzu.edu.tw/Linux/raspbian/raspbian/ jessie main contrib non-free rpi
+#" /etc/apt/sources.list
+#apt-get update
+#
+#apt-get install vim ctags automake subversion ipython3-notebook python3-dev python3-pip
+#
+#echo "link the vimrc and bashrc"
+#ln -s /home/pi/git/vimrc/pi/pi_vimrc ~/.vimrc
+#rm ~/.bashrc
+#ln -s /home/pi/git/vimrc/pi/bashrc ~/.bashrc
+#
+echo "install chinese font and input"
+apt-get install ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy
+apt-get install scim scim-tables-zh scim-chewing
 
-## python environment
-apt-get install ipython-notebook 
-
-## python web dev framework
-apt-get install python-flask
-
-## compile
-apt-get install gcc-4.8 automake
-
-## tools
-apt-get install subversion 
-
-
-
-#### cp gvimrc and vimrc
-cp ./vimrc ~/.vimrc
-cp ./gvimrc ~/.gvimrc
-
+echo "install other IDE and editor"
+apt-get install gvim-gnome geany geany-plugin-py gedit
