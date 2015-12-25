3fb8d1adcd7f69945eea7301553d6eb6335bc3d5
onionys <onionys@gmail.com>
2015-12-25 09:25:22 +0000

    modified:   pi/install_pi_environment.sh

--- pi/install_pi_environment.sh
+++ pi/install_pi_environment.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-#echo "Add ftp.yzu.edu.tw site into apt source.list"
-#sed -i "1ideb ftp://ftp.yzu.edu.tw/Linux/raspbian/raspbian/ jessie main contrib non-free rpi
-#" /etc/apt/sources.list
-#apt-get update
+echo "Add ftp.yzu.edu.tw site into apt source.list"
+sed -i "1ideb ftp://ftp.yzu.edu.tw/Linux/raspbian/raspbian/ jessie main contrib non-free rpi" /etc/apt/sources.list
+apt-get update
 #
-#apt-get install vim ctags automake subversion ipython3-notebook python3-dev python3-pip
+apt-get install vim ctags automake subversion ipython3-notebook python3-dev python3-pip
 #
-#echo "link the vimrc and bashrc"
-#ln -s /home/pi/git/vimrc/pi/pi_vimrc ~/.vimrc
-#rm ~/.bashrc
-#ln -s /home/pi/git/vimrc/pi/bashrc ~/.bashrc
+echo "link the vimrc and bashrc"
+ln -s /home/pi/git/vimrc/pi/pi_vimrc ~/.vimrc
+rm ~/.bashrc
+ln -s /home/pi/git/vimrc/pi/bashrc ~/.bashrc
 #
 echo "install chinese font and input"
 apt-get install ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy
 apt-get install scim scim-tables-zh scim-chewing
 
 echo "install other IDE and editor"
-apt-get install gvim-gnome geany geany-plugin-py gedit
+apt-get install vim-gnome geany geany-plugin-py gedit
